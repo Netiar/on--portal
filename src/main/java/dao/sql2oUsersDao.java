9333ea433cfa5b4a3e5b004a274f6f0411e0b783
Raindrop <collins.odinga@student.moringaschool.com>
2022-05-16 05:38:36 +0300

    ft: added features

---
+++ src/main/java/dao/sql2oUsersDao.java
@@ -0,0 +1,29 @@
+package dao;
+
+
+import models.Users;
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
+
+public class sql2oUsersDao implements UsersDao {
+    private final Sql2o sql2o;
+
+    public sql2oUsersDao(Sql2o sql2o) {
+        this.sql2o = sql2o;
+    }
+
+    @Override
+    public void add(Users user) {
+        String sql = "INSERT INTO users (username, password, role) VALUES (:username, :password, :role)";
+        try (Connection conn = sql2o.open()) {
+            int id = (int) conn.createQuery(sql, true)
+                    .bind(user)
+                    .executeUpdate()
+                    .getKey();
+            user.setId(id);
+        } catch (Sql2oException ex) {
+            System.out.println(ex);
+        }
+    }
+}
