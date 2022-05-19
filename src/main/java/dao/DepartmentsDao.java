e85bc9380b845dd42399b9154141a7b5f6636298
name <collins.odinga@student.moringaschool.com>
2022-05-19 05:50:08 +0300

    rf: refactored code

--- src/main/java/dao/DepartmentsDao.java
+++ src/main/java/dao/DepartmentsDao.java
@@ -1,7 +1,21 @@
 package dao;
 
+import models.Departments;
+
 public interface DepartmentsDao {
     //CRUD
+    void add(Departments department);
+
+    Departments findById(int id);
+
+    void update(int id, Departments department);
+
+    void deleteById(int id);
+
+
+
+
+
 
 
 
