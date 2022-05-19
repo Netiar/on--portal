fc8a9f7e45da4c51764319a11c84bf72e8bcd072
name <collins.odinga@student.moringaschool.com>
2022-05-19 05:51:52 +0300

    rf: refactored code

--- src/main/java/dao/sql2oDepartmentsDao.java
+++ src/main/java/dao/sql2oDepartmentsDao.java
@@ -1,4 +1,52 @@
 package dao;
 
-public class sql2oDepartmentsDao {
+import models.Departments;
+import org.sql2o.Connection;
+
+public class sql2oDepartmentsDao  implements DepartmentsDao{
+    @Override
+    public void add(Departments department) {
+        String sql = "INSERT INTO departments (name, description) VALUES (:name, :description)";
+        try (Connection con = DB.sql2o.open()) {
+            int id = (int) con.createQuery(sql, true)
+                    .addParameter("name", department.getName())
+                    .addParameter("description", department.getDescription())
+                    .executeUpdate()
+                    .getKey();
+            department.setId(id);
+        }
+    }
+
+    @Override
+    public Departments findById(int id) {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM departments WHERE id = :id";
+            return con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Departments.class);
+        }
+    }
+
+    @Override
+    public void update(int id, Departments department) {
+        String sql = "UPDATE departments SET name = :name, description = :description WHERE id = :id";
+        try (Connection con = DB.sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .addParameter("name", department.getName())
+                    .addParameter("description", department.getDescription())
+                    .executeUpdate();
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE FROM departments WHERE id = :id";
+        try (Connection con = DB.sql2o.open()) {
+            con.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+
+    }
 }
