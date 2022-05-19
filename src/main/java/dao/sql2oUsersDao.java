68ab94981aebfec4cd7f610f171431913a0ddb53
name <collins.odinga@student.moringaschool.com>
2022-05-19 04:45:56 +0300

    ft: added features

--- src/main/java/dao/sql2oUsersDao.java
+++ src/main/java/dao/sql2oUsersDao.java
@@ -15,7 +15,8 @@ public class sql2oUsersDao implements UsersDao {
 
     @Override
     public void add(Users user) {
-        String sql = "INSERT INTO users (name,position,role) VALUES (:name, :position, :role)";
+        String sql = "INSERT INTO users (name,rank,staffRole,dept_id) VALUES (:name, :rank, :staffRole, :dept_id)";
+        String sql2 = "SELECT * from departments WHERE id=:dept_id";
         try (Connection conn = sql2o.open()) {
             int id = (int) conn.createQuery(sql, true)
                     .bind(user)
@@ -24,6 +25,54 @@ public class sql2oUsersDao implements UsersDao {
             user.setId(id);
         } catch (Sql2oException ex) {
             System.out.println(ex);
+        }
+    }
+
+    @Override
+    public Users findById(int id) {
+        try (Connection conn = sql2o.open()) {
+            return conn.createQuery("SELECT * FROM users WHERE id = :id")
+                    .addParameter("id", id)
+                    .executeAndFetchFirst(Users.class);
+        }
+    }
+
+    @Override
+    public Users findByName(String name) {
+        try (Connection conn = sql2o.open()) {
+            return conn.createQuery("SELECT * FROM users WHERE name = :name")
+                    .addParameter("name", name)
+                    .executeAndFetchFirst(Users.class);
+        }
+    }
+
+    @Override
+    public void update(int id, Users user) {
+        String sql = "UPDATE users SET name = :name, rank = :rank, staffRole = :staffRole, dept_id = :dept_id WHERE id = :id";
+        try (Connection conn = sql2o.open()) {
+            conn.createQuery(sql)
+                    .bind(user)
+                    .executeUpdate();
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        String sql = "DELETE FROM users WHERE id = :id";
+        try (Connection conn = sql2o.open()) {
+            conn.createQuery(sql)
+                    .addParameter("id", id)
+                    .executeUpdate();
+        }
+    }
+
+    @Override
+    public void deleteByName(String name) {
+        String sql = "DELETE FROM users WHERE name = :name";
+        try (Connection conn = sql2o.open()) {
+            conn.createQuery(sql)
+                    .addParameter("name", name)
+                    .executeUpdate();
         }
     }
 }
