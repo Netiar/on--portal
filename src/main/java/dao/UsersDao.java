e45be6ee53863e4afd6ebac413cccb4f03a2f372
name <collins.odinga@student.moringaschool.com>
2022-05-19 04:44:51 +0300

    ft: added features

--- src/main/java/dao/UsersDao.java
+++ src/main/java/dao/UsersDao.java
@@ -6,5 +6,18 @@ public interface UsersDao {
     //CRUD
     void add(Users user);
 
+    Users findById(int id);
+
+    Users findByName(String name);
+
+    void update(int id, Users user);
+
+    void deleteById(int id);
+
+    void deleteByName(String name);
+
+    //Other methods
+
+
 
 }
