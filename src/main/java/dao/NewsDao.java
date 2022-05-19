e292ba1a05edc432662d352bc185062345dcb134
name <collins.odinga@student.moringaschool.com>
2022-05-19 04:44:22 +0300

    ft: added features

--- src/main/java/dao/NewsDao.java
+++ src/main/java/dao/NewsDao.java
@@ -1,4 +1,23 @@
 package dao;
 
+import models.News;
+
 public interface NewsDao {
+    //CRUD
+    void add(News news);
+
+    News findById(int id);
+
+    News findByTitle(String title);
+
+    News findByTitle();
+
+    void update(int id, News news);
+
+    void deleteById(int id);
+
+    void deleteByTitle(String title);
+
+    //Other methods
+
 }
