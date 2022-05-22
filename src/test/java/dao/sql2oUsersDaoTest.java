dce71741d58304fcc5e143dbdd03204346c69ed0
name <collins.odinga@student.moringaschool.com>
2022-05-22 13:41:21 +0300

    test:testing models and dao's

--- src/test/java/dao/sql2oUsersDaoTest.java
+++ src/test/java/dao/sql2oUsersDaoTest.java
@@ -8,8 +8,8 @@ import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
 
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class sql2oUsersDaoTest {
     sql2oUsersDao dao = new sql2oUsersDao();
@@ -20,43 +20,34 @@ class sql2oUsersDaoTest {
     public void setUp() throws Exception {
         DB.sql2o = new Sql2o("jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:sql/createH2.sql'", "", "");
         conn = DB.sql2o.open();
-     }
-     @After
-        public void tearDown() throws Exception {
-            conn.close();
-        }
-
-
-
-    @Test
-    public void add() {
-        Users user = new Users("name", "rank", "staffRole", 1);
-        dao.add(user);
-        assertEquals(1, user.getId());
-
-
-
     }
 
-
-
-    @Test
-     void getAll() {
-
+    @After
+    public void tearDown() throws Exception {
+        conn.close();
     }
 
     @Test
-    void findById() {
-
+    public void deleteByName_instantiatesCorrectly_true() {
+        Users users = new Users("name", "rank", "staffRole", 1);
+        assertEquals(true, users instanceof Users);
     }
 
     @Test
-    void update() {
-
+    public void getAll_returnsAllUsers_true() {
+        Users users = new Users("name", "rank", "staffRole", 1);
+        users.save(2);
+        Users users2 = new Users("name2", "rank2", "staffRole2", 2);
+        users2.save(2);
+        assertNotEquals(2, Users.getAll().size());
     }
 
     @Test
-    void deleteById() {
+    public void  add_addsNewMentor_true(){
+        Users users = new Users("name", "rank", "staffRole", 1);
+        int id =users.getId();
+        users.save(2);
+        assertEquals(id, users.getId());
 
     }
 
