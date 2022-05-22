66a6f80073cc99e38685b901dfd301f9d04cd264
name <collins.odinga@student.moringaschool.com>
2022-05-22 13:49:33 +0300

    rf:refactored code

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,16 +6,6 @@ public class App {
 
     public static  void main(String[] args) {
 
-        Sql2oDepartmentDao departmentDao;
-        Sql2oNewsDao newsDao;
-        Sql2oUserDao userDao;
-        Gson gson = new Gson();
-        Connection conn;
-
-        departmentDao = new Sql2oDepartmentDao(DB.sql2o);
-        newsDao = new Sql2oNewsDao(DB.sql2o);
-        userDao = new Sql2oUserDao(DB.sql2o);
-        conn = DB.sql2o.open();
 
     }
   // Still working on the app
